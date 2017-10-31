22716798ee6b0c2993e1a7ddc215c270f4ec6760
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-10-30 19:40:38 -0500

    jokes rendering on the page

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,13 @@ app.controller('apicontroller', function($http){
     var ac = this;
     console.log('controller running');
     
-
-})
+    getJoke();
+    function getJoke() {
+        $http.get('https://api.chucknorris.io/jokes/random').then(function(response){
+        //console.log('response', response);
+        ac.chuckNorrisJoke = response.data.value;
+        //console.log(ac.chuckNorrisJoke);
+        
+    })
+    }
+})  
